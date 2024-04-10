1bf58625d04baf08ce9dab2ea8932411cf639cb1
AlexandruDabu <alexandrufelix2020@gmail.com>
2024-04-10 23:06:49 +0300

    added checkbox for list

--- 05-travel-list/starter1/src/App.js
+++ 05-travel-list/starter1/src/App.js
@@ -15,11 +15,15 @@ function App() {
         setItems((items) => [...items].filter((item) => item.id !== id));
     }
 
+    function handleToggleItem(id) {
+        setItems((items) => items.map((item) => item.id === id ? {...item, packed: !item.packed} : item ))
+    }
+
     return(
         <div className="app">
         <Logo/>
         <Form onAddItems={handleAdd}/>
-        <PackingList items={items} onDelete={handleRemove}/>
+        <PackingList items={items} onDelete={handleRemove} onToggle ={handleToggleItem}/>
         <Stats/>
         </div>
     )
--- 05-travel-list/starter1/src/HomePage/PackingList.jsx
+++ 05-travel-list/starter1/src/HomePage/PackingList.jsx
@@ -1,26 +1,28 @@
 import initialItems from "../data"
 import { useState } from "react"
 
-function Item({item, onDelete}){
+function Item({item, onDelete, onToggle}){
     
     function handleRemove(e){
         e.preventDefault();
         onDelete(item.id);
     }
 
-    return <li><span style ={item.packed ? {textDecoration: 'line-through'} : {}}>
+    return <li>
+        <input type='checkbox' value={item.packed} onChange={() => onToggle(item.id)}/>
+        <span style ={item.packed ? {textDecoration: 'line-through'} : {}}>
         {item.quantity} {item.description}
         </span>
         <button onClick={handleRemove}>❌</button>
         </li>
 }
 
-function PackingList({items, onDelete}) {
+function PackingList({items, onDelete, onToggle}) {
     return( 
     <div className="list">
         <ul>
             {items.map((item) =>
-            (<Item item={item} onDelete={onDelete} key={item.id}/>))}
+            (<Item onToggle={onToggle} item={item} onDelete={onDelete} key={item.id}/>))}
         </ul>
     </div>
     )
